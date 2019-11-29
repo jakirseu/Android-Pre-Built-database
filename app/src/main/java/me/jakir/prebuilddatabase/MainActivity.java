ad5ff9bf9b4cd1648aa3d23117cd1aeb1cacfe29
Jakir Hossain <jakirseu@gmail.com>
2019-11-29 15:20:57 +0400

    update projec

--- app/src/main/java/me/jakir/prebuilddatabase/MainActivity.java
+++ app/src/main/java/me/jakir/prebuilddatabase/MainActivity.java
@@ -2,10 +2,11 @@ package me.jakir.prebuilddatabase;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     String DB_NAME = "hello.db";
